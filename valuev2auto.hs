df74f9c85e1fe9f0f87285f14ec7f2430a180079
Martin Muggli <martin.muggli@gmail.com>
2015-12-15 12:09:45 -0700

    fixed missing data in output file

--- valuev2auto.hs
+++ valuev2auto.hs
@@ -1,10 +1,11 @@
+--import Debug.Trace
 import Text.ParserCombinators.Parsec as PS
 import System.IO
 import System.Environment
 import Data.List
 import qualified Data.ByteString.Lazy as BL
 import Data.Binary.Put
-import Control.Applicative as A
+--import Control.Applicative as A
     
 valouevFile :: GenParser Char st [(String, String, String, [Float])]
 valouevFile =
@@ -45,8 +46,8 @@ frag_delim :: [Float]
 frag_delim = [100000.0]
 
 dumpnode :: (Float, Int) -> Put
-dumpnode (label, value) = do
-  putWord32host $ fromIntegral $ quantize $ round label * 1000
+dumpnode (lab, value) = do
+  putWord32host $ fromIntegral $ quantize $ round lab * 1000
   putWord32host $ fromIntegral value
 
 enumerate :: [Float] -> [(Float, Int)]
@@ -87,14 +88,19 @@ quantize :: Int -> Int
 quantize val = if (val `mod` bin_size) < (bin_size `quot` 2)
                then val - (val `mod` bin_size)
                else val - (val `mod` bin_size) + bin_size
-                                
-max_skipnode = 3
+
+max_skipnode :: Int                                
+max_skipnode = 5
+
+initnode :: (Float, Int)
 initnode = (0.0, 2100000000) -- fixme, should be 2**32 - 1 but not sure if unsigned clean
+
+finalnode :: (Float, Int)
 finalnode = (0.0, 0)
 
 --FIXME Edge and Node should probably be types
 dumpEdgePiece :: (Float, Int) -> Put
-dumpEdgePiece (label, pos) = do putWord32host $ fromIntegral pos
+dumpEdgePiece (_, pos) = do putWord32host $ fromIntegral pos
 
 dumpEdge :: [(Float, Int)] -> Put
 dumpEdge enum_nodes = do let actions = fmap dumpEdgePiece enum_nodes
@@ -110,6 +116,7 @@ dumpGraph :: [[Float]] -> [[(Float, Int)]] -> Put
 dumpGraph all_skipnodes edges = do dumpNodes all_skipnodes
                                    dumpEdges edges -- FIXME: TODO
 
+main :: IO ()
 main = do
   args <- getArgs
   let fname = (head args)
@@ -122,25 +129,33 @@ main = do
    Right x -> print $ show $ intercalate frag_delim $ fmap extract_frags x
   case parseOM contents of
    Left x -> print $ show $ x
-   Right x -> BL.hPut ohdl $ runPut $ dumpGraph all_skipnodes edges
-              where  nodes = intercalate frag_delim $ fmap extract_frags x
-                     skipnode_list n = nth_skipnodes n nodes
-                     all_skipnodes = fmap skipnode_list [max_skipnode..1]
+   Right x -> sequence_ [show_stats,  dump_file ]
+              where  show_stats = print $ "nodes: " ++ (show num_all_nodes) ++ " edges: " ++ (show (length edges)) 
+                     dump_file = BL.hPut ohdl $ runPut $ dumpGraph all_skipnodes edges
+                     nodes = intercalate frag_delim $ fmap extract_frags x
+                     skipnode_list n = take ((length nodes) - (n - 1)) $ nth_skipnodes n nodes
+                     all_skipnodes = fmap skipnode_list $ reverse [1..max_skipnode]
                      all_enumerated_skipnodes = enumerateNodes all_skipnodes
                      num_all_nodes = length $ concat all_enumerated_skipnodes
                      enumerated_initnode = (0.0, 0)
                      enumerated_finalnode = (0.0, num_all_nodes)
+                     
                      all_rights :: [[(Float, Int)]]
-                     all_rights = ([[enumerated_initnode]] ++  (rights all_enumerated_skipnodes))
+                     all_rights = [[enumerated_initnode]] ++ (rights all_enumerated_skipnodes)
+
                      all_lefts :: [[(Float, Int)]]
-                     all_lefts = ((lefts all_enumerated_skipnodes) ++ [[enumerated_finalnode]]) 
+                     all_lefts = (lefts all_enumerated_skipnodes) ++ [[enumerated_finalnode]]
+
                      junction_nodes_pair :: [     ([(Float, Int)], [(Float, Int)])    ]
                      junction_nodes_pair = zip all_rights all_lefts
+
                      product ::  ([(Float, Int)], [(Float, Int)])   -> [[(Float, Int)]]
-                     product (left, right) = sequence [left, right]
+                     product (left, right) =  sequence [left, right]
+
                      --products a = concat $ map product a 
                      edge_lists :: [[[(Float, Int)]]]
                      edge_lists = fmap product  junction_nodes_pair
+
                      edges = concat $ edge_lists
   
   hClose ohdl  
@@ -149,13 +164,13 @@ main = do
 -- can probably generate the middle portion of the left and right ends of edges using: take, drop, and transpose
 -- 'sequence' will form the cartesian product of lists of lists
 
--- takes a list of skip lists and returns a list of heads; assumes shortest list first : FIXME: check for or eliminate this assumption
+-- takes a list of skip lists and returns a list of heads; assumes shortest list first and decreasing size : FIXME: check for or eliminate this assumption
 -- lefts means all the skipnodes have their left edge aligned, which actually means they form the right side of a junction
 lefts :: [[a]] -> [[a]]
 lefts [] = []
-lefts [[]] = []
-lefts ([]:skip_lists) = fmap head skip_lists : lefts ( fmap tail skip_lists )
-lefts skip_lists = fmap head skip_lists : lefts ( fmap tail skip_lists )
+lefts [[]] = [[]]
+lefts ([]:skip_lists) =  fmap head skip_lists : lefts ( fmap tail skip_lists )
+lefts skip_lists =  fmap head skip_lists : lefts ( fmap tail skip_lists )
 
 -- FIXME: rights is inefficient, as it forces the construction and retension of intermediate lists, this could be done lazily and allow the used heads to be GC'd
 rights :: [[a]] -> [[a]]
@@ -164,5 +179,6 @@ rights skip_lists = reverse $ lefts $ fmap reverse skip_lists
 windows :: Int -> [a] -> [[a]]
 windows m = transpose . take m . tails
 
+nth_skipnodes :: Num b => Int -> [b] -> [b]
 nth_skipnodes n nodes  = fmap sum $ windows n nodes
 
